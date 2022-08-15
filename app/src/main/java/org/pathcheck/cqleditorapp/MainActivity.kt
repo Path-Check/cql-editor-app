3d36cf1c679aa57a296ec9332d2b64240be0398b
Vitor Pamplona <vitor@vitorpamplona.com>
2022-08-15 11:17:41 -0400

    Activating layout binding

--- app/src/main/java/org/pathcheck/cqleditorapp/MainActivity.kt
+++ app/src/main/java/org/pathcheck/cqleditorapp/MainActivity.kt
@@ -1,61 +1,54 @@
 package org.pathcheck.cqleditorapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
 import org.cqframework.cql.cql2elm.CqlCompiler
 import org.cqframework.cql.cql2elm.CqlCompilerException
 import org.cqframework.cql.cql2elm.LibraryBuilder
 import org.cqframework.cql.cql2elm.LibraryManager
 import org.cqframework.cql.cql2elm.ModelManager
+import org.pathcheck.cqleditorapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-  lateinit var tvResults: TextView;
-  lateinit var etCode: EditText;
+  private lateinit var binding: ActivityMainBinding
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    binding.etCode.setText(assets.open("ImmunityCheck-1.0.0.cql").bufferedReader().readText())
+    binding.btCompile.setOnClickListener { compile() }
+  }
 
-    tvResults = findViewById(R.id.tvResults)
-    etCode = findViewById(R.id.etCode)
+  @OptIn(ExperimentalTime::class)
+  private fun compile() {
+    val (compiler, elapsed) = measureTimedValue {
+      val modelManager = ModelManager()
+      val libraryManager = LibraryManager(modelManager)
 
-    val button: Button = findViewById(R.id.btCompile)
-    button.setOnClickListener {
-      compile();
+      val compiler = CqlCompiler(modelManager, libraryManager)
+      compiler.run(
+        binding.etCode.text.toString(),
+        CqlCompilerException.ErrorSeverity.Info,
+        LibraryBuilder.SignatureLevel.All
+      )
+      compiler
     }
+
+    binding.tvResults.text = report(compiler, elapsed)
   }
 
-  override fun onResume() {
-    super.onResume()
-    etCode.setText(assets.open("ImmunityCheck-1.0.0.cql").bufferedReader().readText());
-  }
-
-  fun compile() {
-    val start = System.currentTimeMillis();
-
-    val modelManager = ModelManager()
-    val libraryManager = LibraryManager(modelManager)
-
-    val compiler = CqlCompiler(modelManager, libraryManager)
-    compiler.run(
-      etCode.text.toString(),
-      CqlCompilerException.ErrorSeverity.Info,
-      LibraryBuilder.SignatureLevel.All
-    )
-
-    val timeSpent = (System.currentTimeMillis()-start)/1000.0f;
-
+  private fun report(compiler: CqlCompiler, elapsed: Duration): String {
     val results = StringBuilder()
-    results.appendLine("Compiled with ${compiler.errors.size} errors in ${timeSpent} seconds\n")
+    results.appendLine("Compiled with ${compiler.errors.size} errors in ${elapsed.inWholeMilliseconds/1000.0f} seconds\n")
 
     compiler.errors.forEach {
-      results.appendLine("[${it.locator.getStartLine()}] ${it.message}");
+      results.appendLine("[${it.locator.startLine}] ${it.message}")
     }
 
-    tvResults.setText(results.toString())
+    return results.toString()
   }
 }
