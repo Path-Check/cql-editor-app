6c8dceecbaf6264a16399aede6b59e52c91f66e8
Vitor Pamplona <vitor@vitorpamplona.com>
2022-08-26 21:54:32 -0400

    Adding PlanDefinition test cases.

---
+++ app/src/androidTest/java/org/opencds/cqf/cql/evaluator/plandefinition/r4/PlanDefinitionProcessorTests.java
@@ -0,0 +1,80 @@
+package org.opencds.cqf.cql.evaluator.plandefinition.r4;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.fasterxml.jackson.databind.ext.CoreXMLDeserializers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.xml.namespace.QName;
+
+@RunWith(AndroidJUnit4.class)
+public class PlanDefinitionProcessorTests extends PlanDefinitionProcessorTestBase {
+    @Test
+    public void testChildRoutineVisit() {
+        test(
+                "child-routine-visit/child_routine_visit_patient.json",
+                "child-routine-visit/child_routine_visit_plan_definition.json",
+                "ChildRoutineVisit-PlanDefinition-1.0.0",
+                "Patient/ChildRoutine-Reportable",
+                null,
+                "child-routine-visit/child_routine_visit_careplan.json"
+        );
+    }
+
+    @Test
+    public void testHelloWorld() {
+        test(
+                "hello-world/hello-world-patient-data.json",
+                "hello-world/hello-world-patient-view-bundle.json",
+                "hello-world-patient-view",
+                "helloworld-patient-1",
+                "helloworld-patient-1-encounter-1",
+                "hello-world/hello-world-careplan.json"
+        );
+    }
+
+    CoreXMLDeserializers a;
+    QName b;
+
+    @Test
+    public void testOpioidRec10PatientView() {
+        /* NOTE:
+            All dynamicValues with the path equaling action.extension have been removed from the
+            plandefinition until the issue in the link https://github.com/DBCG/cqf-ruler/issues/539 has been resolved.
+         */
+        test(
+                "opioid-Rec10-patient-view/opioid-Rec10-patient-view-patient-data.json",
+                "opioid-Rec10-patient-view/opioid-Rec10-patient-view-bundle.json",
+                "opioidcds-10-patient-view",
+                "example-rec-10-patient-view-POS-Cocaine-drugs",
+                "example-rec-10-patient-view-POS-Cocaine-drugs-prefetch",
+                "opioid-Rec10-patient-view/opioid-Rec10-patient-view-careplan.json"
+        );
+    }
+
+    @Test
+    public void testRuleFiltersNotReportable() {
+        test(
+                "rule-filters/tests-NotReportable-bundle.json",
+                "rule-filters/RuleFilters-1.0.0-bundle.json",
+                "plandefinition-RuleFilters-1.0.0",
+                "NotReportable",
+                null,
+                "rule-filters/NotReportableCarePlan.json"
+        );
+    }
+
+    @Test
+    public void testRuleFiltersReportable() {
+        test(
+                "rule-filters/tests-Reportable-bundle.json",
+                "rule-filters/RuleFilters-1.0.0-bundle.json",
+                "plandefinition-RuleFilters-1.0.0",
+                "Reportable",
+                null,
+                "rule-filters/ReportableCarePlan.json"
+        );
+    }
+}
